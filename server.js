74834cced5105ba0e53ea9fe8b3bd6fb76065861
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-10-17 22:31:51 +1100

    server.js static path added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
   
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
